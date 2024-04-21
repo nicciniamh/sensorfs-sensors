f1debfbf9e36f73bac2524da7d63b9794e17f0f2
Nicole Stevens <nicci@ducksfeet.com>
2024-04-21 14:15:39 -0700

    After changing psen, I removed the unused class PseudoSensorx

--- dflib/psen.py
+++ dflib/psen.py
@@ -3,37 +3,6 @@ import sys
 import json
 import time
 
-class PsuedoSensorx:
-	'''
-	Since the data collection is done by a daemon process, this class provides 
-	the rquired sensor interface to read sensor data from a ramdisk. 
-	'''
-	def __init__(self,**kwargs):
-		self.sensor = None
-		self.host = None
-		self.base_path = None
-		for k,v in kwargs.items():
-			setattr(self,k,v)
-
-	def read(self):
-		'''
-		read data, allowing for race conditions on sensor files
-		'''
-		tries = 0
-		dpath = os.path.join(self.base_path,f'{self.host}-{self.sensor}.json')
-		data = None
-		while not data:
-			try:
-				with open(dpath) as f:
-					data = json.load(f)
-			except json.decoder.JSONDecodeError:
-				tries += 1
-				data = None
-				time.sleep(.3)
-				if tries > 5:
-					return None
-		return data
-			
 class PsuedoSensor:
 	'''
 	Since the data collection is done by a daemon process, this class provides 
